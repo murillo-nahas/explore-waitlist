69175d88009bc93f0559ac6121bda0ad649dbaf4
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 08:28:56 -0300

    feat(wip): spinner

--- src/app/components/Toast/Toast.tsx
+++ src/app/components/Toast/Toast.tsx
@@ -14,15 +14,17 @@ export function Toast({
   borderColor,
 }: ToastProps) {
   return (
-    <div
-      className={`flex items-center gap-4 mb-6 p-4 rounded-lg w-96 bg-transparent border ${borderColor} hover:bg-black hover:cursor-pointer`}
-    >
-      <span className="text-2xl">{emoji}</span>
-      <div>
-        <p className="font-bold text-white">
-          {title} {firstname}
-        </p>
-        <p className="text-sm text-label">{message}</p>
+    <div className="fixed inset-0 bg-gray bg-opacity-90 flex items-center justify-center">
+      <div
+        className={`flex items-center bg-black gap-4 mb-6 p-4 rounded-lg w-96 bg-transparent border ${borderColor}`}
+      >
+        <span className="text-2xl">{emoji}</span>
+        <div>
+          <p className="font-bold text-white">
+            {title} {firstname}
+          </p>
+          <p className="text-sm text-label">{message}</p>
+        </div>
       </div>
     </div>
   );
