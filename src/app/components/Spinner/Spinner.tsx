c62a1fa566868064db384e29c52ecf779fc4286a
Murillo Nahás <murillonahas@hotmail.com>
2024-03-26 08:28:44 -0300

    feat(wip): toast

--- src/app/components/Spinner/Spinner.tsx
+++ src/app/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ export function Spinner() {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+        className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -17,6 +17,7 @@ export function Spinner() {
           fill="currentFill"
         />
       </svg>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 }
